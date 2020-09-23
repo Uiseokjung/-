577aa13f05c5d95a87474ee7bbe392f8de601dac
Uiseokjung <70683738+Uiseokjung@users.noreply.github.com>
2020-09-23 17:23:33 +0900

    Update 05_practice_2.ino.ino
    second commit

--- 05_practice_2.ino.ino
+++ 05_practice_2.ino.ino
@@ -1,1 +1,15 @@
 # 과제제출
+#define PIN_LED 7
+
+void setup() {
+  pinMode(7, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(7, HIGH);
+    delay(200);
+    digitalWrite(7, LOW);
+    delay(200);    
+    digitalWrite(7, HIGH);
+    delay(200);
+}
